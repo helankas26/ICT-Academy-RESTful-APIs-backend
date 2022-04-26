9f6eaec7bf287d62325bc70178c12b94bb748211
Helanka Singhapurage <helankas26@gmail.com>
2022-04-26 15:40:45 +0530

    :technologist: decouple morph type fully qualified class name

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Relation::enforceMorphMap([
+            'Student' => 'App\Models\Student',
+            'Employee' => 'App\Models\Employee',
+            'Staff' => 'App\Models\Staff',
+            'Teacher' => 'App\Models\Teacher',
+        ]);
     }
 }
