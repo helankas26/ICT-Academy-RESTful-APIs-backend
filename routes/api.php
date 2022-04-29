44455974b1e6fb1b35962d9639e7c6f2217591c4
Helanka Singhapurage <helankas26@gmail.com>
2022-04-29 23:03:56 +0530

    :technologist: change api route & add RouteHelper

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\RouteHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('v1.0')
+    ->group(function (){
+        RouteHelper::includeRouteFiles(__DIR__.'/api/v1.0');
+//      require_once __DIR__.'/api/v1.0/branches.php';
+    });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
---
+++ routes/api/v1.0/branches.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware([
+
+])
+    ->prefix('branch')
+    ->as('branches.')
+    ->group(function (){
+
+    });
