79e9bcbe4f1eb06de54af9544713cbca305dad7f
JananiNimeshika <jananimeshi@gmail.com>
2022-05-15 12:33:32 +0530

    :sparkles: implement Teacher module CRUD operations
    Teacher module CRUD and API routes

--- app/Repositories/Implementation/TeacherRepository.php
+++ app/Repositories/Implementation/TeacherRepository.php
@@ -2,53 +2,158 @@
 
 namespace App\Repositories\Implementation;
 
+use App\Http\Requests\StoreTeacherRequest;
+use App\Http\Requests\UpdateTeacherRequest;
+use App\Models\Teacher;
 use App\Repositories\Interfaces\TeacherRepositoryInterface;
+use App\Services\Interfaces\IDGenerate\IDGenerateServiceInterface;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class TeacherRepository implements TeacherRepositoryInterface
 {
+    /**
+     * @var IDGenerateServiceInterface
+     */
+    private IDGenerateServiceInterface $IDGenerateService;
 
     /**
-     * @return mixed
+     * @param IDGenerateServiceInterface $IDGenerateService
      */
-    public function getAllTeachers()
+    public function __construct(IDGenerateServiceInterface $IDGenerateService)
     {
-        // TODO: Implement getAllTeachers() method.
+        $this->IDGenerateService = $IDGenerateService;
     }
 
     /**
-     * @param array $request
+     * @param Request $request
      * @return mixed
+     * @throws Exception
      */
-    public function createTeacher(array $request)
+    public function getAllTeachers(Request $request)
     {
-        // TODO: Implement createTeacher() method.
+        $teachers = Teacher::query()->with(['employee', 'employee.person'])
+            ->join('employees', 'teachers.teacherID', 'employees.employeeID')
+            ->join('people', 'employees.employeeID', 'people.personID')
+            ->where('people.status', data_get($request, 'status'))
+            ->get();
+
+        if ($teachers->isEmpty()){
+            throw new Exception('Failed to retrieve Teacher');
+        }
+
+        return $teachers;
     }
 
     /**
-     * @param $teacher
+     * @param StoreTeacherRequest $request
      * @return mixed
+     * @throws Throwable
      */
-    public function getTeacherById($teacher)
+    public function createTeacher(StoreTeacherRequest $request)
     {
-        // TODO: Implement getTeacherById() method.
+        return DB::transaction( function () use ($request){
+
+            DB::statement('SET foreign_key_checks = 0;');
+
+            $teacher = Teacher::query()->create([
+                'teacherID' => $this->IDGenerateService->teacherID(),
+                'qualification' => data_get($request, 'qualification'),
+            ]);
+
+            $teacher->employee()->create([
+                'nic' => data_get($request, 'nic'),
+                'title' => data_get($request, 'title'),
+            ]);
+
+            $teacher->employee->person()->create([
+                'firstName' => data_get($request, 'firstName'),
+                'lastName' => data_get($request, 'lastName'),
+                'dob' => data_get($request, 'dob'),
+                'sex' => data_get($request, 'sex'),
+                'telNo' => data_get($request, 'telNo'),
+                'address' => data_get($request, 'address'),
+                'email' => data_get($request, 'email'),
+                'status' => data_get($request, 'status'),
+                'joinedDate' => data_get($request, 'joinedDate'),
+            ]);
+
+            DB::statement('SET foreign_key_checks = 1;');
+
+            return $teacher;
+        });
     }
 
     /**
-     * @param array $request
-     * @param $teacher
+     * @param Teacher $teacher
      * @return mixed
      */
-    public function updateTeacher(array $request, $teacher)
+    public function getTeacherById(Teacher $teacher)
     {
-        // TODO: Implement updateTeacher() method.
+        return Teacher::query()->find($teacher);
     }
 
     /**
-     * @param $teacher
+     * @param UpdateTeacherRequest $request
+     * @param Teacher $teacher
      * @return mixed
+     * @throws Throwable
      */
-    public function forceDeleteTeacher($teacher)
+    public function updateTeacher(UpdateTeacherRequest $request, Teacher $teacher)
     {
-        // TODO: Implement forceDeleteTeacher() method.
+        return DB::transaction(function () use ($request, $teacher){
+
+            $teacher->update([
+                'qualification' => data_get($request, 'qualification'),
+            ]);
+
+            $teacher->employee->update([
+                'nic' => data_get($request, 'nic'),
+                'title' => data_get($request, 'title'),
+            ]);
+
+            $updated = $teacher->employee->person->update([
+                'firstName' => data_get($request, 'firstName'),
+                'lastName' => data_get($request, 'lastName'),
+                'dob' => data_get($request, 'dob'),
+                'sex' => data_get($request, 'sex'),
+                'telNo' => data_get($request, 'telNo'),
+                'address' => data_get($request, 'address'),
+                'email' => data_get($request, 'email'),
+                'status' => data_get($request, 'status'),
+                'joinedDate' => data_get($request, 'joinedDate'),
+            ]);
+
+            if (!$updated){
+                throw new Exception('Failed to update Teacher: ' . $teacher->teacherID);
+            }
+
+            return $teacher;
+        });
+    }
+
+    /**
+     * @param Teacher $teacher
+     * @return mixed
+     * @throws Throwable
+     */
+    public function forceDeleteTeacher(Teacher $teacher)
+    {
+        return DB::transaction(function () use ($teacher){
+
+            $teacher->delete();
+
+            $teacher->employee->delete();
+
+            $deleted = $teacher->employee->person->delete();
+
+            if (!$deleted) {
+                throw new Exception('Failed to delete Teacher: ' . $teacher->teacherID);
+            }
+
+            return $deleted;
+        });
     }
 }
--- app/Repositories/Interfaces/TeacherRepositoryInterface.php
+++ app/Repositories/Interfaces/TeacherRepositoryInterface.php
@@ -2,11 +2,41 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Http\Requests\StoreTeacherRequest;
+use App\Http\Requests\UpdateTeacherRequest;
+use App\Models\Teacher;
+use Illuminate\Http\Request;
+
 interface TeacherRepositoryInterface
 {
-    public function getAllTeachers();
-    public function createTeacher(array $request);
-    public function getTeacherById($teacher);
-    public function updateTeacher(array $request, $teacher);
-    public function forceDeleteTeacher($teacher);
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function getAllTeachers(Request $request);
+
+    /**
+     * @param StoreTeacherRequest $request
+     * @return mixed
+     */
+    public function createTeacher(StoreTeacherRequest $request);
+
+    /**
+     * @param Teacher $teacher
+     * @return mixed
+     */
+    public function getTeacherById(Teacher $teacher);
+
+    /**
+     * @param UpdateTeacherRequest $request
+     * @param Teacher $teacher
+     * @return mixed
+     */
+    public function updateTeacher(UpdateTeacherRequest $request, Teacher $teacher);
+
+    /**
+     * @param Teacher $teacher
+     * @return mixed
+     */
+    public function forceDeleteTeacher(Teacher $teacher);
 }
