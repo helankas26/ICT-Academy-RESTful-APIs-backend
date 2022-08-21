aa11e45846ac2e85e7be45d5efe3bf2e95c37590
Helanka Singhapurage <helankas26@gmail.com>
2022-08-21 09:11:04 +0530

    :recycle: change route uri in Advance Reporting module

--- routes/api/v1.0/Reporting/advanceReports.php
+++ routes/api/v1.0/Reporting/advanceReports.php
@@ -10,6 +10,6 @@ Route::middleware([
     ->as('advanceReports.')
     ->group(function (){
         Route::get('/advances', [AdvanceReportController::class, 'index'])->name('index');
-        Route::get('/advances/byEmployee', [AdvanceReportController::class, 'indexByEmployeeType'])->name('index.byEmployee');
+        Route::get('/advances/byEmployeeType', [AdvanceReportController::class, 'indexByEmployeeType'])->name('index.byEmployeeType');
         Route::get('/employees/{employee:employeeID}/advances', [AdvanceReportController::class, 'showByEmployee'])->name('show.employee');
     });
